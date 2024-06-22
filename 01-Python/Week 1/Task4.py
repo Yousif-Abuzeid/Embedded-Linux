7e0e6e482d751856fd6f8ee918f0e619672d1fc0
Youssef-Abuzeid <yousifh991@gmail.com>
2024-06-22 18:34:04 +0300

    Task 4 Done

---
+++ 01-Python/Week 1/Task4.py
@@ -0,0 +1,19 @@
+def CalcArea(radius):
+    """
+    Function to calculate the area of a circle
+
+    Parameters:
+    radius(float): radius of the circle to calculate the area
+
+    Returns:
+    float: Returns the area of the circle
+
+    """
+    pi=3.14
+
+    return pi*radius**2
+
+# Example
+
+r=3
+print(CalcArea(r))
