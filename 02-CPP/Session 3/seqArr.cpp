fd0b10634c29714c7af2b59def6e5f0786f78b2a
Youssef-Abuzeid <yousifh991@gmail.com>
2024-08-03 18:27:19 +0300

    seqArr Task

---
+++ 02-CPP/Session 3/seqArr.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+
+template <typename T>
+std::vector<T> seqArr(T start, T end, T step){
+    std::vector<T> arr(((end - start) / (step))+1);
+    std::generate(arr.begin(), arr.end(), [&n = start, step]()  { 
+        int temp = n;
+        n += step; 
+        return temp;});
+    return arr;
+}
+
+int main(){
+    auto arr = seqArr(1, 10, 2);
+    for(auto i : arr){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
