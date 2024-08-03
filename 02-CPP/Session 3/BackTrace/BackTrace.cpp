9ff97c949119b1cb94e5ea6191b6ceeeabab038c
Youssef-Abuzeid <yousifh991@gmail.com>
2024-08-03 21:06:59 +0300

    BackTrace Task

--- 02-CPP/Session 3/BackTrace/BackTrace.cpp
+++ 02-CPP/Session 3/BackTrace/BackTrace.cpp
@@ -3,26 +3,48 @@
 #include <stack>
 #include <vector>
 
+// Define the static member variable
 std::stack<std::string> backTrace::Trace;
 
-void backTrace::Enter(const std::string& funName){
-            Trace.push(funName);
-            std::cout<<"Enter :"<<funName<<std::endl;
-        }
-void backTrace::Exit(){
-            std::cout<<"Exit :"<<Trace.top()<<std::endl;
-            Trace.pop();
-        }
-void backTrace::PrintBackTrace(){
-            std::stack<std::string> temp=Trace;
-            std::vector<std::string> vector_Trace;
-            int count =0;
-            std::cout<<"BackTrace As Follows:"<<std::endl;
-            while (!temp.empty()) {
-                vector_Trace.push_back(temp.top());
-                temp.pop();
-            }
-            for(auto it = vector_Trace.rbegin(); it!=vector_Trace.rend(); it++){
-                std::cout<<count<<"-"<<*it<<std::endl;
-            }
+/**
+ * @brief Adds the function name to the stack trace and logs the entry.
+ * 
+ * @param funName The name of the function to add to the stack trace.
+ */
+void backTrace::Enter(const std::string& funName) {
+    Trace.push(funName);
+    std::cout << "Enter: " << funName << std::endl;
+}
+
+/**
+ * @brief Removes the top function name from the stack trace and logs the exit.
+ */
+void backTrace::Exit() {
+    if (!Trace.empty()) {
+        std::cout << "Exit: " << Trace.top() << std::endl;
+        Trace.pop();
+    }
+}
+
+/**
+ * @brief Prints the current stack trace from the first function entered to the last.
+ */
+void backTrace::PrintBackTrace() {
+    std::stack<std::string> temp = Trace;
+    std::vector<std::string> vector_Trace;
+    int count = 0;
+
+    std::cout << "BackTrace As Follows:" << std::endl;
+
+    // Move elements from stack to vector to maintain order
+    while (!temp.empty()) {
+        vector_Trace.push_back(temp.top());
+        temp.pop();
+    }
+
+    // Print elements from the vector in the original order
+    for (auto it = vector_Trace.rbegin(); it != vector_Trace.rend(); ++it) {
+        std::cout << count << "-" << *it << std::endl;
+        ++count;
+    }
 }
--- 02-CPP/Session 3/BackTrace/BackTrace.hpp
+++ 02-CPP/Session 3/BackTrace/BackTrace.hpp
@@ -1,25 +1,41 @@
-#ifndef  BACKTRACE_HPP
+#ifndef BACKTRACE_HPP
+#define BACKTRACE_HPP
 
-#define  BACKTRACE_HPP
 #include <stack>
 #include <string>
 
-
-#define ENTER_FUNCTION backTrace::Enter (__FUNCTION__)
-#define EXIT_FUNCTION  backTrace::Exit() 
+/// Macro to be placed at the beginning of a function to add it to the backtrace
+#define ENTER_FUNCTION backTrace::Enter(__FUNCTION__)
+/// Macro to be placed at the end of a function to remove it from the backtrace
+#define EXIT_FUNCTION  backTrace::Exit()
+/// Macro to print the current backtrace
 #define PRINT_BT       backTrace::PrintBackTrace()
 
-class backTrace{
+/**
+ * @class backTrace
+ * @brief A class to manage a stack trace for debugging purposes.
+ */
+class backTrace {
 
-    private:
-      static  std::stack<std::string> Trace;
-    public:
-      static  void Enter(const std::string& funName);
-            
-        static void Exit();
-        static void PrintBackTrace();
+private:
+    static std::stack<std::string> Trace; ///< Stack to store function names
 
+public:
+    /**
+     * @brief Enters a function by adding its name to the trace stack.
+     * @param funName The name of the function to add to the trace stack.
+     */
+    static void Enter(const std::string& funName);
+
+    /**
+     * @brief Exits a function by removing its name from the trace stack.
+     */
+    static void Exit();
+
+    /**
+     * @brief Prints the current backtrace.
+     */
+    static void PrintBackTrace();
 };
 
-#endif  //BACKTRACE_HPP
-
+#endif // BACKTRACE_HPP
